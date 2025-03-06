84a9aef2d9590a50d82fa1257b6318b8d97b9b6d
Nicolas Constantin <nicolasconstantin@hotmail.fr>
2025-03-06 18:48:13 +0100

    🚀 Initialisation du projet avec Git

--- App/Models/Ingredient.swift
+++ App/Models/Ingredient.swift
@@ -4,6 +4,8 @@ struct Ingredient: Codable, Identifiable {
     let id: Int
     let nom: String
     let unite: String?
-    let quantite: Int
+    let categorie: String?  // Ajout de la catégorie
+    let recette_id: Int     // Ajout de l'ID de la recette associée
+    var quantite: Int       // ✅ Correction : let -> var pour rendre la quantité modifiable
+    let optionnel: Bool     // Ajout du champ optionnel
 }
-
